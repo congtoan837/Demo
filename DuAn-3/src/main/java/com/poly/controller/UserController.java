8d5166224c536f656da5d49dbcfb42cb0f236348
ToanLC4 <ToanLC4@fpt.com.vn>
2021-02-17 18:19:34 +0700

    a

--- DuAn-3/src/main/java/com/poly/controller/UserController.java
+++ DuAn-3/src/main/java/com/poly/controller/UserController.java
@@ -1,27 +1,48 @@
 package com.poly.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.poly.model.Product;
 import com.poly.services.ProductService;
 
-@Controller
+@RestController
 public class UserController {
 	@Autowired
 	private ProductService productService;
 	
 	@GetMapping("/")
-	public String index(ModelMap model) {
+	public ModelAndView index(ModelMap model) {
 		List<Product> list = (List<Product>) productService.findAll();
 		model.addAttribute("Product", list);
-		return "user/index";
+		ModelAndView mav = new ModelAndView("user/index");
+		return mav;
 	}
+	
+	
+	@PostMapping("/api")
+	  public ResponseEntity<List<Product>> getAllTutorials() {
+	    try {
+	      List<Product> tutorials = new ArrayList<Product>();
+     
+	      tutorials = (List<Product>) productService.findAll();	      
+
+	      return new ResponseEntity<>(tutorials, HttpStatus.OK);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
 	
 	@GetMapping("/about")
 	public String about() {
--- DuAn-3/src/main/java/com/poly/security/SecurityConfig.java
+++ DuAn-3/src/main/java/com/poly/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/css/**", "/js/**", "/images/**", "/webfonts/**").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
