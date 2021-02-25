78d364b4acea75b63b4775f61a26332ce1d6e8c9
ToanLC4 <ToanLC4@fpt.com.vn>
2021-02-25 23:32:08 +0700

    Revert "a"
    This reverts commit 2a8e2fec3b4ca2072038962261202fd8a82a1a30.

--- src/main/java/com/poly/controller/RestfulAPI.java
+++ src/main/java/com/poly/controller/RestfulAPI.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -258,7 +257,7 @@ public class RestfulAPI {
 	// API BLOG //
 	
 	// API ACCOUNT //
-	@GetMapping("/listaccount")
+	@PostMapping("/listaccount")
 	List<Admins> listaccount() {
 		return (List<Admins>) adminRepository.findAll();
 	}
