2a8e2fec3b4ca2072038962261202fd8a82a1a30
ToanLC4 <ToanLC4@fpt.com.vn>
2021-02-25 23:28:13 +0700

    a

--- src/main/java/com/poly/controller/RestfulAPI.java
+++ src/main/java/com/poly/controller/RestfulAPI.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -257,7 +258,7 @@ public class RestfulAPI {
 	// API BLOG //
 	
 	// API ACCOUNT //
-	@PostMapping("/listaccount")
+	@GetMapping("/listaccount")
 	List<Admins> listaccount() {
 		return (List<Admins>) adminRepository.findAll();
 	}
