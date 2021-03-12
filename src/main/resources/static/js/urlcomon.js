344ea1f7f8c6c26e5b0b11c24f38c1c87fd58e5b
ToanLC4 <ToanLC4@fpt.com.vn>
2021-03-13 01:01:28 +0700

    a

--- src/main/resources/static/js/urlcomon.js
+++ src/main/resources/static/js/urlcomon.js
@@ -1,2 +1,3 @@
 var API_URL = 'https://demojava5.herokuapp.com';
-var API_URL_LOCAL = 'http://localhost:8080';
+//var API_URL_LOCAL = 'http://localhost:8080';
+var API_URL_LOCAL = 'https://webdemodoan.herokuapp.com';
