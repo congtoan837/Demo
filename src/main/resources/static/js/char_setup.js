be44df7b93d2d8e53203cd9a944b41cfdddd5510
ToanLC4 <ToanLC4@fpt.com.vn>
2021-04-16 06:12:27 +0700

    a

--- src/main/resources/static/js/char_setup.js
+++ src/main/resources/static/js/char_setup.js
@@ -1,65 +1,268 @@
-Highcharts.chart('container', {
-    chart: {
-        type: 'column'
+var a = [];
+const b = [
+    {
+        "timeStart": "2021/4/1",
+        "timeEnd": "2021/4/5",
+        "category": "Ao"
     },
-    title: {
-        text: 'Monthly Average Rainfall'
+    {
+        "timeStart": "2021/4/5",
+        "timeEnd": "2021/4/10",
+        "category": "Giay"
     },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
+    {
+        "timeStart": "2021/4/10",
+        "timeEnd": "2021/4/15",
+        "category": "Giay"
     },
-    xAxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ],
-        crosshair: true
+    {
+        "timeStart": "2021/4/15",
+        "timeEnd": "2021/4/20",
+        "category": "Giay"
     },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Rainfall (mm)'
-        }
+    {
+        "timeStart": "2021/4/20",
+        "timeEnd": "2021/4/25",
+        "category": "Giay"
     },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    {
+        "timeStart": "2021/4/25",
+        "timeEnd": "2021/4/30",
+        "category": "Giay"
+    }
+];
 
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
+$(document).ready(function () {
+    loadData1();
+    loadData2();
+    loadData3();
+    loadData4();
+    loadData5();
+    loadData6();
+    setTimeout(() => {
+        run();
+    }, 500);
 });
+
+function loadData1() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/1",
+                "timeEnd": "2021/4/5",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[0] = data;
+            }
+        });
+    });
+}
+
+function loadData2() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/5",
+                "timeEnd": "2021/4/10",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[1] = data;
+            }
+        });
+    });
+}
+
+function loadData3() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/10",
+                "timeEnd": "2021/4/15",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[2] = data;
+            }
+        });
+    });
+}
+
+function loadData4() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/15",
+                "timeEnd": "2021/4/20",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[3] = data;
+            }
+        });
+    });
+}
+
+function loadData5() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/20",
+                "timeEnd": "2021/4/25",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[4] = data;
+            }
+        });
+    });
+}
+
+function loadData6() {
+
+    b.forEach((item, index) => {
+        $.ajax({
+            cache: false,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/count",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "timeStart": "2021/4/25",
+                "timeEnd": "2021/4/30",
+                "category": "Giay"
+            }),
+            dataType: "json",
+            error: function () {
+
+            },
+            success: function (data) {
+                a[5] = data;
+            }
+        });
+    });
+}
+
+function run() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Doanh Thu Theo Tháng'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            title: {
+                text: 'Ngày'
+            },
+            categories: [
+                '1-5',
+                '5-10',
+                '10-15',
+                '15-20',
+                '20-25',
+                '25-30'
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Số lượng (SP)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} SP</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Giày',
+            data: a
+
+        }, {
+            name: 'Áo',
+            data: [0,0,0,0,0,0]
+
+        }, {
+            name: 'Nón',
+            data: [0,0,0,0,0,0]
+
+        }]
+    });
+}
--- src/main/resources/static/js/user/checkout.js
+++ src/main/resources/static/js/user/checkout.js
@@ -1,4 +1,8 @@
+var total = 0;
+var a = 0;
+var listItem = [];
 $(document).ready(function() {
+	loadradio()
     if(localStorage.getItem("token") !== null){
         loaditem();
     }else{
@@ -66,19 +70,169 @@ function loaditem() {
         },
         success: function(data) {
             if (data.length > 0) {
+                listItem = data;
                 data.map((item, index) => {
                     let price = item.product.price.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
                     let totalPrice = (item.product.price * item.quantity).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+                    total += item.product.price * item.quantity;
                     var str = $(` 	<div class="media mb-2 border-bottom">
                                         <div class="media-body"> <a href="detail/${item.product.id}"> ${item.product.name}</a>
                                             <div class="small text-muted">Giá: ${price} <span class="mx-2">|</span> Số lượng: ${item.quantity} <span class="mx-2">|</span> Tổng tiền: ${totalPrice}</div>
                                         </div>
                                     </div>`);
                     $('#dataTable').append(str);
+                    $('#totalprice').html(total.toLocaleString('it-IT', { style: 'currency', currency: 'VND' }));
+                    $('#thanhtien').html(total.toLocaleString('it-IT', { style: 'currency', currency: 'VND' }));
+                    $('#order').html(str);
                 });
             }
         }
 
     });
 
+}
+
+function loadradio() {
+	$.ajax({
+		cache: false,
+		type: "POST",
+		headers: {
+			Authorization: 'Bearer ' + localStorage.getItem("token")
+		},
+		url: API_URL + "/api/listpayment",
+		contentType: "application/json;charset=UTF-8",
+		dataType: "json",
+		error: function (request) {
+
+		},
+		success: function (data) {
+			dataProduct = data;
+			$('#datatable').html("");
+			data.map((item, index) => {
+				if(index = 1){
+					a = "checked";
+				}
+				var str = $(`<div class="custom-control custom-radio">
+                                    <input id="${item.id}" value="${item.id}" name="paymentMethod" type="radio" class="custom-control-input" ${a} required>
+                                    <label class="custom-control-label" for="paypal">${item.name}</label>
+                                </div>`);
+				$('#radio').append(str);
+			});
+
+		}
+
+	});
+
+}
+
+function insert() {
+    var customerId = 3;
+    var promotion = $('#coupon').val();
+    var status = "Pending"
+    var paymentId = $("#radio input[name= 'paymentMethod']:checked").val();
+
+    {
+        $.ajax({
+            cache: true,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/neworder",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "customerId": customerId,
+                "promotion": promotion,
+                "status": status,
+                "paymentId": paymentId,
+            }),
+            dataType: "json",
+            error: function(request) {
+                toastr.error("fail");
+            },
+            success: function(data) {
+                let id = data.id;
+                listItem.map((item, index) => {
+                    $.ajax({
+                        cache: true,
+                        type: "POST",
+                        headers: {
+                            Authorization: 'Bearer ' + localStorage.getItem("token")
+                        },
+                        url: API_URL + "/api/neworderdetail",
+                        contentType: "application/json;charset=UTF-8",
+                        data: JSON.stringify({
+                            "productId": item.product.id,
+                            "quantity": item.quantity,
+                            "orderId": id
+                        }),
+                        dataType: "json",
+                        error: function(request) {
+                            toastr.error("fail");
+                        },
+                        success: function(request) {
+
+                        }
+                    });
+                });
+                clearCart();
+                toastr.success("Success");
+            }
+        });
+    }
+}
+
+function couponCheck() {
+    var coupon = $('#coupon').val();
+
+    {
+        $.ajax({
+            cache: true,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/findpromo",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "coupon": coupon
+            }),
+            dataType: "json",
+            error: function(request) {
+                $('#promo').html('-');
+                $('#thanhtien').html((total).toLocaleString('it-IT', { style: 'currency', currency: 'VND' }));
+            },
+            success: function(data) {
+                a = (data.percents / 100) * total
+                $('#promo').html(a.toLocaleString('it-IT', { style: 'currency', currency: 'VND' }));
+                $('#thanhtien').html((total-a).toLocaleString('it-IT', { style: 'currency', currency: 'VND' }));
+            }
+        });
+    }
+}
+
+function clearCart() {
+    var cartId = localStorage.getItem("cart")
+
+    {
+        $.ajax({
+            cache: true,
+            type: "POST",
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token")
+            },
+            url: API_URL + "/api/removeall",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify({
+                "cartId": cartId
+            }),
+            dataType: "json",
+            error: function(request) {
+
+            },
+            success: function(data) {
+
+            }
+        });
+    }
 }
